e165a91e355b420a86e41c56e8a6fd212fd79c5d
Estelle Sillari-Owen <emso18@gmail.com>
2017-11-26 23:29:45 -0500

    finalized styling & js

--- simon.js
+++ simon.js
@@ -13,41 +13,51 @@ function randomColor(){
 function addColor(){
   var newColor = randomColor();
   colorArray.push(newColor);
-  console.log(colorArray);
-  lightMove();
+  setTimeout(function(){
+    computerMove(0);
+  }, 1000);
 }
 
-function lightMove(){
-  for(var each in colorArray){
-    switch(colorArray[each]){
+function computerMove(index){
+  if(index !== colorArray.length){
+    switch(colorArray[index]){
       case "red":
         $(".red").addClass("selected");
+        redSound.play();
         break;
       case "green":
         $(".green").addClass("selected");
+        greenSound.play();
         break;
       case "blue":
         $(".blue").addClass("selected");
+        blueSound.play();
         break;
       case "yellow":
         $(".yellow").addClass("selected");
+        yellowSound.play();
         break;
     }
     setTimeout(function(){
       $(".board").removeClass("selected");
-    }, 1000);
+      setTimeout(function(){
+        computerMove(index + 1);
+      },500);
+    }, 500);
   }
-  playerTurn = true;
+  else {
+    $(".game").addClass("player");
+    playerTurn = true;
+  }
 }
 
 function playerMove(color){
-  if(playerTurn){
-    playerArray.push(color);
-    if(playerArray.length === colorArray.length){
-      playerTurn = false;
-      compareMoves();
-    }
+  playerArray.push(color);
+  if(playerArray.length === colorArray.length){
+    playerTurn = false;
+    $(".game").removeClass("player");
   }
+  compareMoves();
 }
 
 function compareMoves(){
@@ -56,30 +66,48 @@ function compareMoves(){
     if(playerArray[i] === colorArray[i]){
       matches += 1;
     }
+    else if(playerArray[i] !== colorArray[i] && strict === true){
+      playerTurn = false;
+      $(".game").removeClass("player");
+      playerArray = [];
+      $(".counter").text("!!");
+      setTimeout(function(){
+        reset();
+        addColor();
+      },2000);
+      break;
+    }
+    else if(playerArray[i] !== colorArray[i] && strict === false){
+      playerTurn = false;
+      $(".game").removeClass("player");
+      playerArray = [];
+      $(".counter").text("!!");
+      setTimeout(function(){
+        $(".counter").text(count);
+        computerMove(0);
+      },2000);
+      break;
+    }
   }
-  playerArray = [];
   if(matches === colorArray.length){
+    playerArray = [];
     count += 1;
     $(".counter").text(count);
     gameStatus();
   }
-  else if(matches !== colorArray.length && strict === true){
-    alert("Bummer, you lost!");
-    reset();
-    addColor();
-  }
-  else if(matches !== colorArray.length && strict === false){
-    alert("Oops, you made a mistake!");
-    lightMove();
-  }
 }
 
 function gameStatus(){
-  if(count === 21){
-    alert("Congrats, you win!!");
-    reset();
+  if(count === 5){
+    $(".counter").text("🎉");
+    setTimeout(function(){
+      reset();
+      addColor();
+    },2000);
   }
-  addColor();
+  else {
+    addColor();
+  }
 }
 
 function reset(){
@@ -87,17 +115,49 @@ function reset(){
   count = 1;
   playerArray = [];
   playerTurn = false;
+  strict = false;
+  $(".game").removeClass("player");
   $(".counter").text(count);
+  $(".strict-off").show();
+  $(".strict-on").hide();
 }
 
 $(document).ready(function(){
   $(".reset").hide();
+  $(".reset-label").hide();
+  $(".strict-on").hide();
 
   $(".start").click(function(){
     $(".reset").show();
+    $(".reset-label").show();
     $(".start").hide();
+    $(".start-label").hide();
     $(".counter").text(count);
+    greenSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+    redSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+    blueSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+    yellowSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+    greenSound.play();
+    greenSound.pause();
+    redSound.play();
+    redSound.pause();
+    blueSound.play();
+    blueSound.pause();
+    yellowSound.play();
+    yellowSound.pause();
     addColor();
+  });
+
+  $(".strict-off").click(function(){
+    $(".strict-off").hide();
+    $(".strict-on").show();
+    strict = true;
+  });
+
+  $(".strict-on").click(function(){
+    $(".strict-on").hide();
+    $(".strict-off").show();
+    strict = false;
   });
 
   $(".reset").click(function(){
@@ -106,26 +166,34 @@ $(document).ready(function(){
   });
 
   $(".red").click(function(){
-    playerMove("red");
+    if(playerTurn){
+      var redSoundClone = redSound.cloneNode();
+      redSoundClone.play();
+      playerMove("red");
+    }
   });
 
   $(".green").click(function(){
-    playerMove("green");
+    if(playerTurn){
+      var greenSoundClone = greenSound.cloneNode();
+      greenSoundClone.play();
+      playerMove("green");
+    }
   });
 
   $(".blue").click(function(){
-    playerMove("blue");
+    if(playerTurn){
+      var blueSoundClone = blueSound.cloneNode();
+      blueSoundClone.play();
+      playerMove("blue");
+    }
   });
 
   $(".yellow").click(function(){
-    playerMove("yellow");
-  });
-
-  $(".strict-off").click(function(){
-    strict = false;
-  });
-
-  $(".strict-on").click(function(){
-    strict = true;
+    if(playerTurn){
+      var yellowSoundClone = yellowSound.cloneNode();
+      yellowSoundClone.play();
+      playerMove("yellow");
+    }
   });
 });
